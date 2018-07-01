61c63b3c167ff453fab407434e58d0e9776d699f
bafunky <40708358+bafunky@users.noreply.github.com>
2018-07-01 10:10:28 -0500

    Update basictank.js

--- basictank.js
+++ basictank.js
@@ -1,6 +1,8 @@
 float[] fishx= {50.0, 150.0, 200.0, 300.0, 400.0, 450.0, 500.0, 550.0, 700.0, 750.0};
 float[] speed= {1.0, 0.8, 0.4, 1.1, 1.05, 1.3, 1.5, 1.2, 0.9, 0.6};
 int[] fishy= {5, 50, 280, 100, 240, 105, 20, 220, 90, 180};
+color[] fishcolor= {color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255)), color( random(0,255), random(0,255), random(0,255))};
+
 
 
 void setup(){
@@ -14,17 +16,21 @@ fill(200,10,0);
 
 void draw(){
 
-  background(30,60,150);
+  background(160,170,255);
   
 for(int i=0; i<10; i=i+1){
-ellipse(fishx[i], fishy[i], 50,20);
-fishx[i]= fishx[i] + speed[i];
+  fill(fishcolor[i]);
+  ellipse(fishx[i], fishy[i], 50,20);
+  triangle(fishx[i]-40, fishy[i]+15, fishx[i]-40, fishy[i]-15, fishx[i]-20, fishy[i]);
+  fill(0,0,0);
+  ellipse(fishx[i]+15, fishy[i]+5, 3,3);
+  fishx[i]= fishx[i] + speed[i];
 
-if(fishx[i] > 800){
-fishx[i]= -50.0;
-fishy[i]= round(random(0,350));
-
-}
+  if(fishx[i] > 800){
+    fishx[i]= -50.0;
+    fishy[i]= round(random(0,350));
+    fishcolor[i]= color( random(0,255), random(0,255), random(0,255));
+  }
 
 
 }
